51adc88a95f1580cb0c8c725fe5dd89aaf1df969
Hex <tec@Hex6666.local>
2022-08-29 18:32:27 +0100

    first commit

---
+++ hydrogen.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain: 'kicksnake-nextjs.myshopify.com',
+    storefrontToken: 'd8502c31946bb33ca249439b394dbb9d',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
---
+++ src/components/product/Countdown.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
+export default class Countdown extends Component {
+
+  render() {
+    return(
+        <div className="elfsight-app-072a7279-855a-4462-9a6f-20b3dc764927">
+
+        <Helmet>
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+    </Helmet>
+</div>
+    )
+  }
+
+}
